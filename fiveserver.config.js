87134d9f0241a32d4b2e88ac92e5fe9e12444bd1
Dere Shiracc <voidtsundere@gmail.com>
2024-08-01 20:23:01 -0300

    Footer + CSS Styling

--- fiveserver.config.js
+++ fiveserver.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    highlight: true, // enable highlight feature
-    injectBody: true, // enable instant update
-    remoteLogs: true, // enable remoteLogs
+    highlight: false, // enable highlight feature
+    injectBody: false, // enable instant update
+    remoteLogs: false, // enable remoteLogs
     remoteLogs: "yellow", // enable remoteLogs and use the color yellow
     injectCss: true, // disable injecting css
-    navigate: true, // enable auto-navigation
+    navigate: false, // enable auto-navigation
   };
